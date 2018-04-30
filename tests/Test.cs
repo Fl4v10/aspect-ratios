c166b2efb18c2c8da83699ea8553f04065d854d1
Fl4v1o <flavio.thebigboss@gmail.com>
2018-04-29 22:05:43 -0300

    Início

---
+++ tests/Test.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using web.Services;
+using Web.Models;
+using Web.Repository;
+using Xunit;
+
+namespace Tests
+{
+    public class Test
+    {
+        [Fact]
+        public async Task Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
